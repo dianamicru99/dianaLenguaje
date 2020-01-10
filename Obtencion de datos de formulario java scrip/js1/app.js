0498fd699f96c7a9b2be96d2b934d53bf78663cb
dianamicru99 <56721094+dianamicru99@users.noreply.github.com>
2020-01-10 11:07:18 +0100

    ok

---
+++ Obtencion de datos de formulario java scrip/js1/app.js
@@ -0,0 +1,6 @@
+function getData()
+{
+    var nombre=document.getElementById("nombre").value;
+    var edad=document.getElementById("edad").value;
+    alert("Tu nombre es : "+nombre+ " y tienes " +edad+ " años");  
+}
